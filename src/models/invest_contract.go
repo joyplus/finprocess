369e5f87e66a5a5beec017b1a15991a991b346da
xlmine <moon@xlmine.local>
2015-10-19 23:14:37 +0800

    modify

--- src/models/invest_contract.go
+++ src/models/invest_contract.go
@@ -1,10 +1,13 @@
 package models
 
-type invest_contract struct {
+import (
+	"github.com/astaxie/beego/orm"
+)
 
-	Id              int
+type Invest_Contract struct {
+	Id              int            `orm:pk;auto`
 	Master_id       int
-	Description     string
+	Description     string        `orm:size(100)`
 	Amount_min      int
 	Amount_max      int
 	Duration_min    int
@@ -14,4 +17,21 @@ type invest_contract struct {
 	Rate            float32
 	Early_terminate int
 	Risk_rank       int
+}
+
+type Invest_ContractDAO struct {
+}
+
+func (this *Invest_ContractDAO) Save(i *Invest_Contract) (int, error) {
+	o := orm.NewOrm()
+	id, err := o.Insert(i)
+	return id, err
+}
+
+func (this *Invest_ContractDAO) Get(id int) (m *Invest_Contract) {
+	o := orm.NewOrm()
+	i := &Invest_Contract{}
+	qs := o.QueryTable("fin_p2p_master")
+	qs.Filter("id", id).One(i)
+	return i
 }
--- src/models/master.go
+++ src/models/master.go
@@ -1,14 +1,35 @@
 package models
 
-type master struct {
-	Id                   int
-	Name                 string
-	Official_url         string
-	Product_description  string
-	Invest_area          string
-	Duration_description string
+import (
+	"github.com/astaxie/beego/orm"
+)
+
+type Master struct {
+	Id                   int       	   `orm:pk;auto`
+	Name                 string        `orm:size(50)`
+	Official_url         string        `orm:size(500)`
+	Product_description  string        `orm:size(100)`
+	Invest_area          string        `orm:size(50)`
+	Duration_description string        `orm:size(100)`
 	Platform_est_rate    float32
 	Platform_min_amount  float32
-	Platform_icon_url    string
+	Platform_icon_url    string        `orm:size(10)`
 	Risk_rank            int
+}
+
+type MasterDAO struct {
+}
+
+func (this *MasterDAO) Save(c *Master) (int, error) {
+	o := orm.NewOrm()
+	id, err := o.Insert(c)
+	return id, err
+}
+
+func (this *MasterDAO) Get(id int) (m *Master) {
+	o := orm.NewOrm()
+	master := &Master{}
+	qs := o.QueryTable("fin_p2p_master")
+	qs.Filter("id", id).One(master)
+	return master
 }
